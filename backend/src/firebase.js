e5bf5b5b43b318876f98de9405f61072c01776d3
dexisback <dontjustthinkexecutejee2@gmail.com>
2026-01-16 23:20:44 +0530

    removed hardcoded file parsing so render could read it lol

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -1,9 +1,8 @@
 import admin from "firebase-admin";
-import fs from "fs";
+// import fs from "fs";
 
 //this below line reads the firebase-admin.json file, parses it, verifies that we have the correct credentials, export this thing from this file and hence allow the whole backend to have the access to the admin of this faayabase project
-const serviceAccount= JSON.parse(fs.readFileSync("./firebase-admin.json", "utf-8"));
-
+const serviceAccount= JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
