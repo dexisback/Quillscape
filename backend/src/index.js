c4abd04a802cf7bb2fd31b23fdf9b9d47791c75d
dexisback <dontjustthinkexecutejee2@gmail.com>
2026-01-24 00:29:40 +0530

    no

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,5 +29,5 @@ app.get("/health", (req, res)=>{
     res.status(200).send("alive and runnnin")
 })
 
-const PORT= process.env.port || 3000 ;
+const PORT= process.env.PORT || 3000 ;
 app.listen(PORT, ()=>{console.log("Up and runnin' ✅")})
