073292d59dcdd2b6e565960705af40dc154ae866
Amaan <texamaan86@gmail.com>
2026-02-06 03:01:38 +0530

    something i alwyas wanted to add

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import Navbar from '../components/landing/Navbar';
-import HeroSection from '../components/landing/HeroSection';
-import Footer from '../components/landing/Footer';
+import { useEffect } from 'react'
+import Navbar from '../components/landing/Navbar'
+import HeroSection from '../components/landing/HeroSection'
+import Footer from '../components/landing/Footer'
+
 
 export default function Landing() {
+
+//thankyou shubhash choudhary :)
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_BASE_URL + '/blogs/public').catch(() => { })
+  }, [])
+
   return (
     <main className="w-full min-h-screen">
       <Navbar />
       <HeroSection />
       <Footer />
     </main>
-  );
+  )
 }
