b2b07753d80fa960bf898b30a3eba8688f17c0d3
Amaan <texamaan86@gmail.com>
2026-03-21 00:36:46 +0530

    remove legacy react/vite, migration complete

--- apps/react-frontend/src/lib/slate/index.js
+++
@@ -1,3 +0,0 @@
-export { markdownToSlate, slateToMarkdown } from './markdown.js'
-export { withMarkdownShortcuts, withImages } from './plugins.js'
-export { renderElement, renderLeaf } from './elements.jsx'
